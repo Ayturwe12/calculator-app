98ec1cb692dcf43ec3e0ef8e99255b28891b2c31
ahmetguler <ahmetguler20@trakya.edu.tr>
2024-12-14 14:14:46 +0300

    i added some notes

--- tkinter1.py
+++ tkinter1.py
@@ -6,7 +6,7 @@ e = Entry(root,width=35, borderwidth=5)
 e.grid(row=0, column=0, columnspan=3, padx=10,pady=10)
 
 
-def button_click(number):
+def button_click(number):                          #useful buttons
     current= e.get()
     e.delete(0, END)
     e.insert(0,str(current)+str(number))
@@ -43,7 +43,7 @@ def button_all(process):
     math= "{}".format(process)
     f_num = float(first_number)
     e.delete(0, END)
-
+# there are the buttons here
 button_1= Button(root,text="1", padx=40,pady=20,command= lambda: button_click(1))
 button_2= Button(root,text="2", padx=40,pady=20,command= lambda:button_click(2))
 button_3= Button(root,text="3", padx=40,pady=20,command= lambda:button_click(3))
@@ -67,6 +67,7 @@ button_square_root=Button(root,text="²√", padx=37,pady=20,command= lambda:but
 button_square=Button(root,text="x²", padx=38,pady=20,command= lambda:button_all("square"))
 button_exponent=Button(root,text="^", padx=42,pady=20,command= lambda:button_all("exponent"))
 
+# griding buttons on the app.
 button_1.grid(row= 3, column=0)
 button_2.grid(row= 3, column=1)
 button_3.grid(row= 3, column=2)
